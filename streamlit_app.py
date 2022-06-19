7ca0994daf235946ad1e3bc6c154056493a5b2e3
sanjayguptas <sanjayguptas@gmail.com>
2022-06-19 18:40:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Break Fast Menu')
 streamlit.text('Omega 3 and Blue Berry oat meal')
+streamlit.text('Kale Speanech rocket smoothee')
 streamlit.text('Hard Boiled free range eggs')
